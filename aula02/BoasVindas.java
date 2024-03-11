5ee4724a1c9a9c2b73b9af927f274136b05028cf
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-03-11 01:53:25 -0300

    aula 02

---
+++ aula02/BoasVindas.java
@@ -0,0 +1,13 @@
+/* Programa que imprime
+   uma mensagem de
+   boas vindas          */
+
+public class BoasVindas {
+   public static void main(String args[]) 
+   {
+      System.out.printf("Seja bem-vindo \na programacao %S!\n", "Java");   
+   }
+}
+
+
+
