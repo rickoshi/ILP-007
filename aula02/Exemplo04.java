e93cb511fcd97449138ade9d4b9a55aee83b258d
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-03-17 03:10:21 -0300

    Exemplo04_LeituraString

---
+++ aula02/Exemplo04.java
@@ -0,0 +1,24 @@
+// LeituraString.java
+// Programa que lê e exibe cadeia de caracteres
+
+import java.util.Scanner;
+
+public class Exemplo04 {
+    public static void main(String[]args) {
+        Scanner input = new Scanner(System.in);
+        String frase; // Não é um tipo primitivo
+
+        System.out.println("Digite uma frase:");
+        frase = input.nextLine();
+
+        System.out.printf("A frase lida e %s\n", frase);
+        input.close();
+    }
+}
+
+/*
+Entrada de cadeia de caracteres - String
+Variável com o tipo String;
+método da variável Scanner "nextLine()";
+formato de saída String "%s";
+*/
