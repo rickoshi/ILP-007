59f5689e58af0332ace3d2381afe9acf24f43230
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-03-18 16:17:44 -0300

    Exemplos

---
+++ aula04/Circulo.java
@@ -0,0 +1,23 @@
+public class Circulo {
+
+   private double raio;
+   
+   public Circulo(double r) {
+      setRaio(r);
+   }
+   
+   public void setRaio(double r){
+      if (r < 0)
+         System.out.println("O raio nao pode ser negativo.");
+      else
+         raio = r;
+   }
+   
+   public double getRaio(){
+      return raio;
+   }
+
+   public void exibeDados(){
+      System.out.println("Raio: " + getRaio());
+   }
+}
---
+++ aula04/TesteCirculo.java
@@ -0,0 +1,7 @@
+public class TesteCirculo {
+   public static void main(String[] args){
+      Circulo c = new Circulo(12);
+      // c.setRaio(1);
+      c.exibeDados();
+   }
+}
