e76a61d58d416a5c12634506b8389b5d97b1c244
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-03-17 21:49:17 -0300

    Exemplo01

---
+++ aula03/Exemplo01.java
@@ -0,0 +1,72 @@
+package aula03;
+
+// import java.util.Scanner;
+
+public class Exemplo01 {
+    public static void main(String[] args) {}
+}
+
+
+
+/* 
+ * Aula 3
+ * Operadores relacionais
+ * Operadores lógicos
+ * Estrutura de decisão
+ * Estrutura de Repetição
+ */
+
+
+/*
+ * Operadores relacionais
+ * Operador     Resultado
+ *  x == y      V se x igual a y
+ *  x != y      V se x diferente de y
+ *  x < y       V se x menor que y
+ *  x > y       V se x maior que y
+ *  x <= y      V se x menor ou igual a y
+ *  x >= y      V se x maior ou igual a y
+ */
+
+/*
+ * Operadores lógicos
+ * Operador     Resultado
+ *   !x         V se x for falso
+ *  x && y      V se x e y ambos verdadeiros
+ *  x || y      V se x ou y (ou ambos) verdadeiros
+ */
+
+/*
+ * Estrutura de decisão simples
+ * if (condição)
+ *     comando1;
+ * else 
+ *     comando2;
+ * 
+ * if( m >= 7.0 ) 
+ *     System.out.println(“Aprovado”);
+ * else 
+ *     System.out.println(“Reprovado”);
+ */
+
+/*
+ * Condicional aninhada
+ * if( condição1 )
+ *     if (condição2)
+ *         Comando1;
+ *     else
+ *         Comando2;
+ * else 
+ *     Comando3;
+ */
+
+/*
+ * Condicional encadeada
+ * if( condição1 )
+ *     Comando1;
+ * else
+ *     if (condição2)
+ *         Comando2;
+ *     else 
+ *         Comando3;
+ */
