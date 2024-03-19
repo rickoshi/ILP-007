af820b8519402a2a4b4d35f6c7bd41f3f38c1846
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-03-19 01:00:47 -0300

    Exercicio06_Caixa

---
+++ aula03/Exercicio06.java
@@ -0,0 +1,15 @@
+/* Atividade 06 - Caixa
+Um comerciante precisa informatizar o caixa de sua loja. Codifique um programa que:
+Leia uma série de valores correspondendo aos preços das mercadorias compradas por um cliente (o valor zero finaliza a entrada);
+Calcule o valor total;
+Aplique o desconto conforme tabela ao lado;
+Mostre o valor a ser pago pelo cliente. 
+*/
+
+package aula03;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        
+    }
+}
