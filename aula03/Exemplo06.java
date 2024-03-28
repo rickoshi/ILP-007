9ea2da23efeed3268efdf9793c832b3dc2c3df60
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-03-28 03:09:33 -0300

    Exemplo06_Calculadora

---
+++ aula03/Exemplo06.java
@@ -0,0 +1,52 @@
+// Calculadora.java
+// Uso da estrutura de decisão múltipla para implementar uma simples calculadora;
+// O usuário digita uma expressão da forma "val1", "operador", "val2" e o programa fornece-lhe seu valor como resposta
+// Os valores podem ser reais e os operadores aceitos são: +, -, * e /.
+
+package aula03;
+
+import java.util.Scanner;
+
+public class Exemplo06
+{
+    public static void main(String[] args)
+    {
+        Scanner iNum = new Scanner(System.in);  // Entrada para números
+        Scanner iStr = new Scanner(System.in);  // Entrada para operador
+        
+        System.out.println("Digite uma expressao: ");
+        double x = iNum.nextDouble();
+        String s = iStr.nextLine();
+        double y = iNum.nextDouble();
+
+        char op = s.charAt(0); // Converte o operador de String para caracter
+        switch (op) 
+        {
+            case '+': System.out.println("Valor = " + (x+y)); break;
+            case '-': System.out.println("Valor = " + (x-y)); break;
+            case '*': System.out.println("Valor = " + (x*y)); break;
+            case '/': System.out.println("Valor = " + (x/y)); break;
+            default: System.out.println("Operador invalido: " + op); break;
+        }
+        iNum.close();
+        iStr.close();
+    }
+}
+
+/*
+ * Estrutura de decisão múltipla
+ * Usada quando precisamos escolher uma entre várias alternativas previamente definidas
+ * 
+ * switch(exp)
+ * {
+ *  case const1: comando1;
+ *      break;
+ *  case const2: comando2;
+ *      break;
+ *  ...
+ *  case constn: comandon;
+ *      break;
+ *  default: comando;
+ * }
+ * 
+ */
