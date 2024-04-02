9d7ae063a2b458583e422f6318cb07b11c332b52
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-01 23:48:05 -0300

    Exercicio07_ClasseCirculoTeste

---
+++ aula04/Exercicio07_Teste.java
@@ -0,0 +1,11 @@
+package aula04;
+
+public class Exercicio07_Teste {
+    public static void main(String[] args) {
+        Exercicio07 circ = new Exercicio07(8);
+        circ.exibeDados();
+    
+        circ.setRaio(5);
+        circ.exibeDados();
+    }
+}
