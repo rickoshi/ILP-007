d629aab91d42de4950dddb9a732d4bd1d7ea5970
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-07 19:54:30 -0300

    Exercicio08_ContaCorrenteTeste

--- aula04/Exercicio08_Teste.java
+++ aula04/Exercicio08_Teste.java
@@ -1,7 +1,25 @@
 package aula04;
+import java.util.Scanner;
 
 public class Exercicio08_Teste {
     public static void main(String[] args) {
+        Exercicio08 cc1 = new Exercicio08(12345, "Joao da Silva", 0);
+        cc1.verDados();
+        
+        Exercicio08 cc2;
+        cc2 = new Exercicio08(54321, "Maria dos Santos", 500);
+        cc2.verDados();
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.printf("\nValor para deposito em c1: ");
+        double vlr = entrada.nextDouble();
+        cc1.deposito(vlr);
+        cc1.verDados();
+        
+        System.out.printf("\n Valor de saque em c2: ");
+        cc2.saque(entrada.nextDouble());
+        cc2.verDados();
         
     }
 }
