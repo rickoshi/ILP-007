e33cf138f1318dfc8d2b866a2f7cf0a62a8ad95d
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-12 05:30:20 -0300

    Exemplo01

---
+++ aula05/Exemplo01.java
@@ -0,0 +1,28 @@
+package aula05;
+
+public class Exemplo01 {
+
+}
+
+
+
+/*
+ * Sobrecarga de métodos
+ * O que é?
+ * – Recurso que permite que vários métodos, com mesmo nome sejam definidos
+ * – Obrigatório diferentes parâmetros: quantidade, tipos e ordem dos argumentos
+ * – Apesar de ser bastante usada para definir vários construtores, a sobrecarga também pode ser usada para sobrescrever outros métodos.
+ */
+
+/*
+ * Quantos construtores uma classe pode ter?
+ * Nenhum (construtor padrão: não recomendável)
+ * Apenas 1 construtor
+ * Vários construtores!!!
+ * Para isso usaremos o recurso de sobrecarga
+ */
+
+
+
+
+ 
