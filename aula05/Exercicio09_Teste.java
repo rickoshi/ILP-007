6dd68c3262a3b97c4c22bb69001ca86feebedbf1
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-14 18:48:19 -0300

    Exercicio09_ClasseDataTeste

--- aula05/Exercicio09_Teste.java
+++ aula05/Exercicio09_Teste.java
@@ -1,13 +1,13 @@
 package aula05;
 
-public class Exercicio01_Teste {
+public class Exercicio09_Teste {
     public static void main(String[] args)
     {
-        Exercicio01 data1, data2, data3, data4;
-        data1 = new Exercicio01(28, 02, 2024);
-        data2 = new Exercicio01(29, 02, 2024);
-        data3 = new Exercicio01(30, 02, 2024);
-        data4 = new Exercicio01(29, 02, 2025);
+        Exercicio09 data1, data2, data3, data4;
+        data1 = new Exercicio09(28, 02, 2024);
+        data2 = new Exercicio09(29, 02, 2024);
+        data3 = new Exercicio09(30, 02, 2024);
+        data4 = new Exercicio09(29, 02, 2025);
 
         System.out.println(data1.FormatarData());
         System.out.println(data2.FormatarData());
