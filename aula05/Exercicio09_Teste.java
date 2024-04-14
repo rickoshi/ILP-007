be933a47fd57ae7a440ad2d83e6ac32d2ef38d3d
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-14 19:05:58 -0300

    Exercicio09_ClasseDataTeste

--- aula05/Exercicio09_Teste.java
+++ aula05/Exercicio09_Teste.java
@@ -13,5 +13,17 @@ public class Exercicio09_Teste {
         System.out.println(data2.FormatarData());
         System.out.println(data3.FormatarData());
         System.out.println(data4.FormatarData());
+
+        
+        Exercicio09Alternativo d1, d2, d3, d4;
+        d1 = new Exercicio09Alternativo(28, 02, 2024);
+        d2 = new Exercicio09Alternativo(29, 02, 2024);
+        d3 = new Exercicio09Alternativo(30, 02, 2024);
+        d4 = new Exercicio09Alternativo(29, 02, 2025);
+
+        System.out.println(d1.FormatarData());
+        System.out.println(d2.FormatarData());
+        System.out.println(d3.FormatarData());
+        System.out.println(d4.FormatarData());
     }
 }
