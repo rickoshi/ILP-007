edbd34086bae4b0c2048ced1fd48cfa142674385
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-14 14:16:00 -0300

    Exemplo01_Data

--- aula05/Exemplo01.java
+++ aula05/Exemplo01.java
@@ -1,10 +1,51 @@
+// Data.java
+// Implementar uma classe que receba como entrada inteiros, valide o dia, mês e ano e mostre como saída a data formatada
+
 package aula05;
 
 public class Exemplo01 {
+    private int dia;    // 1 a 31, de acordo com o mês
+    private int mes;    // 1 a 12
+    private int ano;    // qualquer ano
+    public Exemplo01(int dia, int mes, int ano)
+    {
+        setData(dia, mes, ano);
+    }
 
+    public void setData(int dia, int mes, int ano)
+    {
+        if (mes > 0 && mes <= 12)
+        {
+            this.mes = mes;
+        }
+        else
+        {
+            mes = 1;
+            System.out.println("Mes: " + mes + " invalido. Configurado mes = 1.");
+        }
+        this.ano = ano;
+        this.dia = checkDia(dia);
+    }
+
+    private int checkDia(int diaTeste)   // Método privado: uso interno da classe
+    {
+        int diaMes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (diaTeste > 0 && diaTeste <= diaMes[mes])
+        {
+            return diaTeste;
+        }
+        else // Esse else pode ser omitido, já que: se a condição for verdadeira, a função retorna o valor e se encerra antes.
+        {
+            System.out.println("Dia " + diaTeste + " invalido. Configurado dia = 1.");
+            return 1;
+        }
+    }
+
+    public String FormatarData()
+    {
+        return dia + "/" + mes + "/" + ano;
+    }
 }
-
-
 
 /*
  * Sobrecarga de métodos
@@ -21,8 +62,3 @@ public class Exemplo01 {
  * Vários construtores!!!
  * Para isso usaremos o recurso de sobrecarga
  */
-
-
-
-
- 
