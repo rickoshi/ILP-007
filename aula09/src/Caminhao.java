006b71a74ba5a06882ffe7090f3d488a4374389e
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-28 20:24:44 -0300

    Classe caminhao

---
+++ aula09/src/Caminhao.java
@@ -0,0 +1,37 @@
+package aula09.src;
+
+public class Caminhao extends Veiculo {
+    private double capacidade;
+    private int numEixos;
+
+    public Caminhao(String modelo, String placa, int anoFabr, double valor, double capacidade, int numEixos) {
+        super(modelo, placa, anoFabr, valor);
+        setCapacidade(capacidade);
+        setNumEixos(numEixos);
+    }
+
+    public void setCapacidade(double capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public void setNumEixos(int numEixos) {
+        this.numEixos = 2;
+        if (numEixos > 2) {
+            this.numEixos = numEixos;
+        }
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+
+    public int getNumEixos() {
+        return numEixos;
+    }
+
+    public void imprimir() {
+        System.out.printf("\nVeiculo: %s\nPlaca: %7s", modelo, placa);
+        System.out.printf("\nFabr: %4d\nCapacidade: %4d", anoFabr, capacidade);
+        System.out.printf("\n%02d Eixos\nR$ %.2f\n", numEixos, valor);
+    }
+}
