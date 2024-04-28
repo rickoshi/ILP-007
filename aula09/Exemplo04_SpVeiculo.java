880956e6d7857607d28668237d6237030c9a7666
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-28 19:23:39 -0300

    Exemplo04_SuperclasseVeiculoProtected

---
+++ aula09/Exemplo04_SpVeiculo.java
@@ -0,0 +1,70 @@
+// VeiculoProtected.java
+// Implementa uma superclasse Veiculo
+
+package aula09;
+
+public class Exemplo04_SpVeiculo
+{
+    // Attributes
+    protected String modelo, placa;
+    protected int anoFabr;
+    protected double valor;
+
+    // Constructor
+    public Exemplo04_SpVeiculo(String pModelo, String pPlaca, int pAnoFabr, double pValor)
+    {
+        setModelo(pModelo);
+        setPlaca(pPlaca);
+        setAnoFabr(pAnoFabr);
+        setValor(pValor);
+    }
+
+    // Setters/Getters
+    public void setModelo(String pModelo) {
+        modelo = pModelo;
+    }
+
+    public String getModelo() {      
+        return this.modelo;  // uso opcional nesse caso
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }  
+
+    public void setAnoFabr(int anoFabr) { 
+        this.anoFabr = anoFabr;
+    }
+    
+    public int getAnoFabr() {
+        return anoFabr;
+    }  
+    
+    public void setValor(double valor)
+    { 
+        if (valor >= 0)
+            this.valor = valor;
+        else
+            this.valor = 0;
+    }
+    
+    public double getValor() {
+        return valor;
+    }  
+
+    // Class methods
+    public void deprecia(float taxa)
+    {
+        setValor(valor - valor * taxa/100);
+    }
+        
+    public void imprime()
+    {
+        System.out.printf(" \nVeiculo: %s\nPlaca: %7s\nAno: %4d\nValor: R$%.2f\n",
+        modelo, placa, anoFabr, valor);
+    }
+}
