14393027cede781c7098e2dde87fccfada078944
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-04-28 21:27:38 -0300

    Teste da classe Veiculo, Carro e Caminhao

--- aula09/src/Teste.java
+++ aula09/src/Teste.java
@@ -3,10 +3,17 @@ package aula09.src;
 public class Teste {
     public static void main(String[] args) 
     {
+        // Objeto da classe Veiculo
+        Veiculo moto;
+        moto = new Veiculo("Yamaha", "AMZ1289", 2023, 19000);
+        moto.imprime();
+        moto.deprecia(5);
+        System.out.println("Veiculo depreciado");
+        moto.imprime();
+
         // Objeto da classe Carro
         Carro carro;
         carro = new Carro("Fiesta", "ABC1678", 2006, 2007, 3, 31000);
-
         carro.imprime();
         carro.deprecia(10);
         System.out.println("Carro depreciado");
@@ -15,13 +22,23 @@ public class Teste {
         // Objeto da classe Caminhao
         Caminhao caminhao;
         caminhao = new Caminhao("Mercedes", "XYZ3456", 2000, 10, 3, 50000);
-
         caminhao.imprime();
         caminhao.deprecia(10);
         System.out.println("Caminhao depreciado");
         caminhao.imprime();
 
         /*
+         * Veiculo: Yamaha
+         * Placa: AMZ1289
+         * Ano: 2023
+         * Valor: R$19000,00
+         * Veiculo depreciado
+         * 
+         * Veiculo: Yamaha
+         * Placa: AMZ1289
+         * Ano: 2023
+         * Valor: R$18050,00
+         * 
          * Veiculo: Fiesta
          * Placa: ABC1678
          * Fabr: 2006
