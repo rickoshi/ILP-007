7fe0d21f2b3d3cb8c151e5dda0a3767371a1a2a5
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-20 02:25:21 -0300

    Subclasse Triangulo

---
+++ aula11/src/Triangulo.java
@@ -0,0 +1,34 @@
+package aula11.src;
+
+public class Triangulo extends FormaGeometrica
+{
+    private double base, altura;
+
+    public Triangulo(double base, double altura) {
+        super.setLados(3);
+        setBase(base);
+        setAltura(altura);
+    }
+
+    public void setBase(double base) {
+        if (base < 0) this.base = 0;
+        else this.base = base;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) this.altura = 0;
+        else this.altura = altura;
+    }
+
+    public double area() {
+        return (base * altura)/2;
+    }
+
+    public void imprime() {
+        System.out.println("Dados do triangulo: ");
+        super.imprime();
+        System.out.println("Base: " + base);
+        System.out.println("Altura: " + altura);
+    }
+
+}
