d08a7e50605e05159201fb32c8975ec0d2befa4b
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-20 02:25:44 -0300

    Teste das classes Circulo, Retangulo e Triangulo

---
+++ aula11/src/TesteFormas.java
@@ -0,0 +1,37 @@
+package aula11.src;
+
+public class TesteFormas {
+    public static void main(String[] args) {
+        Circulo c = new Circulo(5);
+        c.imprime();
+        System.out.println();
+
+        Retangulo r = new Retangulo(3, 4);
+        r.imprime();
+        System.out.println();
+
+        Triangulo t = new Triangulo(3, 4);
+        t.imprime();
+        System.out.println();
+
+        /*
+         * Dados do circulo:
+         * Numero de lados: 0
+         * Area: 0,00
+         * Raio: 0.0
+         * 
+         * Dados do retangulo:
+         * Numero de lados: 4
+         * Area: 12,00
+         * Base: 3.0
+         * Altura: 4.0
+         * 
+         * Dados do triangulo:
+         * Numero de lados: 3
+         * Area: 6,00
+         * Base: 3.0
+         * Altura: 4.0
+         * 
+         */
+    }
+}
