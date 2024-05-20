9fba411eceb9785cb028967cbf922d63572666b4
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-20 02:25:11 -0300

    Subclasse Retangulo

---
+++ aula11/src/Retangulo.java
@@ -0,0 +1,33 @@
+package aula11.src;
+
+public class Retangulo extends FormaGeometrica
+{
+    private double base, altura;
+
+    public Retangulo(double base, double altura) {
+        super.setLados(4);
+        setBase(base);
+        setAltura(altura);
+    }
+
+    public void setBase(double base) {
+        if (base < 0) this.base = 0;
+        else this.base = base;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) this.altura = 0;
+        else this.altura = altura;
+    }
+
+    public double area() {
+        return base * altura;
+    }
+
+    public void imprime() {
+        System.out.println("Dados do retangulo: ");
+        super.imprime();
+        System.out.println("Base: " + base);
+        System.out.println("Altura: " + altura);
+    }
+}
