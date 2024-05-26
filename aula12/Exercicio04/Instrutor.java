aa2f635c041b1d81240fe26a1c325138727fff07
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-25 22:40:56 -0300

    Subclasse Instrutor

---
+++ aula12/Exercicio04/Instrutor.java
@@ -0,0 +1,44 @@
+package aula12.Exercicio04;
+
+import java.lang.Integer;
+
+public class Instrutor extends Funcionario
+{
+    private String disciplina;
+    private Integer horasMensais;
+    private float valorHoraAula;
+
+    public Instrutor(String nome, String mail, String disciplina, Integer horasMensais, float valorHoraAula) {
+        super(nome, mail);
+        setDisciplina(disciplina);
+        setHorasMensais(horasMensais);
+        setValorHoraAula(valorHoraAula);
+    }
+
+    public void setDisciplina(String disc) {
+        this.disciplina = disc;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setHorasMensais(Integer horas) {
+        this.horasMensais = horas;
+    }
+
+    public void setValorHoraAula(float vlr) {
+        this.valorHoraAula = vlr;
+    }
+
+    public float pagamento() {
+        return horasMensais * valorHoraAula;
+    }
+
+    public void exibeDados() {
+        super.exibeDados();
+        System.out.println("Disciplina: " + disciplina);
+        System.out.println("Horas mensais: " + horasMensais);
+        System.out.println("Valor da hora: " + valorHoraAula);
+    }
+}
