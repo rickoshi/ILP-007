5dc2c679c96f48601d2a1a001877f64bd842087c
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-05-25 22:40:46 -0300

    Classe abstrata Funcionario

---
+++ aula12/Exercicio04/Funcionario.java
@@ -0,0 +1,36 @@
+package aula12.Exercicio04;
+
+public abstract class Funcionario 
+{
+    protected String nome;
+    protected String mail;
+
+    public Funcionario(String nome, String mail) {
+        setNome(nome);
+        setMail(mail);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public abstract float pagamento();
+
+    public void exibeDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Mail: " + mail);
+        System.out.println("Pagamento: " + pagamento());
+    }
+}
