7462e10eba740f92ad8ae859a729e29c50d7092a
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-06-12 07:58:15 -0300

    Exemplo02_TesteShowMessage

--- aula15/Exemplo02.java
+++ aula15/Exemplo02.java
@@ -1,3 +1,6 @@
+// TesteShowMessage.java
+// Utiliza do container Caixa de Diálogo para mostrar uma mensagem
+
 package aula15;
 
 import javax.swing.JOptionPane;
@@ -6,7 +9,9 @@ public class Exemplo02 {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Teste dialogo PLAIN_MESSAGE", "Mensagem normal", JOptionPane.PLAIN_MESSAGE);
 //                                      ^                 ^                        ^         		 ^
-//                                   Posição          Mensagem                   Título                Ícone
+//                                   Posição           Mensagem                  Título               Ícone
+// A "Posição" indica o parentComponent, o frame no qual o JOptionPane é mostrado, se for null, apresenta um frame default
+// O Ícone pode ser determinado por String (WARNING_MESSAGE) ou int (2)
     }
 }
 
@@ -22,9 +27,9 @@ public class Exemplo02 {
 /*
  * Tipos de mensagem
  * Define o ícone a ser apresentado:
- * – ERROR_MESSAGE 
- * – INFORMATION_MESSAGE 
- * – WARNING_MESSAGE 
- * – QUESTION_MESSAGE 
- * – PLAIN_MESSAGE
+ * – ERROR_MESSAGE          (0)
+ * – INFORMATION_MESSAGE    (1)
+ * – WARNING_MESSAGE        (2)
+ * – QUESTION_MESSAGE       (3)
+ * – PLAIN_MESSAGE          (4)
  */
