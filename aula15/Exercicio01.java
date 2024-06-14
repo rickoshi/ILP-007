7b95998e2a95843c36f1865a5633de844c1663f1
rickoshi <145235121+rickoshi@users.noreply.github.com>
2024-06-14 01:19:26 -0300

    Exercicio01_SomaDoisInteiros

--- aula15/Exercicio01.java
+++ aula15/Exercicio01.java
@@ -13,8 +13,10 @@ public class Exercicio01 {
     public static void main(String[] args) {
         String input1 = JOptionPane.showInputDialog(null, "Numero 1", "Soma de dois inteiros", JOptionPane.PLAIN_MESSAGE);
         String input2 = JOptionPane.showInputDialog(null, "Numero 2", "Soma de dois inteiros", JOptionPane.PLAIN_MESSAGE);
+
         int num1 = Integer.parseInt(input1);
         int num2 = Integer.parseInt(input2);
+        
         JOptionPane.showMessageDialog(null, "A soma dos dois numeros foi: " + (num1 + num2), "Soma de dois inteiros", JOptionPane.INFORMATION_MESSAGE);
     }
 }
